6d31b0463d4ea85ec9d4707e00c6dd3159d0988c
samprakash.R <samprakash1410@gmail.com>
2024-04-24 08:44:32 +0530

    added

--- csv.py
+++ csv.py
@@ -11,5 +11,28 @@ def write_record(path,p,i,t,r):
         results_writer = csv.writer(file)
         results_writer.writerow([p,i,t,r])
         
+        
+        
+def write_random_data():
+    csv_path = "results.csv"
+    for index in range(3):
+        p = random.randint(1000,10000)
+        i = random.randint(6,24)
+        t = random.randint(1,10)
+        r = random.randint(1000,10000)
+        write_record(csv_path,p,i,t,r)
+
+def read_results(file_path):
+    with open(file_path, 'r') as reader:
+        result_reader = csv.reader(reader,delimiter=',')
+        for record in result_reader:
+            print(record)
+
+
+
+
+if __name__ == "__main__":
+    write_random_data()
+    read_results("results.csv")
 
     
