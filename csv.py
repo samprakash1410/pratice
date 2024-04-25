e1e196388f564c5170f84f9827d3c0494f066382
samprakash.R <samprakash1410@gmail.com>
2024-04-25 17:01:32 +0530

    added

--- csv.py
+++ csv.py
@@ -1,38 +1,23 @@
 import random
-import csv
-import os
 
-def write_record(path,p,i,t,r):
-    # find if file exists
-    file_exists = False
-    if os.path.exists(path):
-        file_exists = True
-    with open(path, 'a') as file:
-        results_writer = csv.writer(file)
-        results_writer.writerow([p,i,t,r])
-        
-        
-        
-def write_random_data():
-    csv_path = "results.csv"
+def write_result(principal, rate, time, result):
+    """This functions appends the results to the end of the file
+    """
+    with open("results.csv", "a") as writer:
+        writer.write(f"{principal},{rate},{time},{result}\n")
+
+def read_results():
+    with open("results.csv", "r") as reader:
+        for line in reader.readlines():
+            print(line)
+
+
+
+if __name__ == "__main__":
     for index in range(3):
         p = random.randint(1000,10000)
         i = random.randint(6,24)
         t = random.randint(1,10)
         r = random.randint(1000,10000)
-        write_record(csv_path,p,i,t,r)
-
-def read_results(file_path):
-    with open(file_path, 'r') as reader:
-        result_reader = csv.reader(reader,delimiter=',')
-        for record in result_reader:
-            print(record)
-
-
-
-
-if __name__ == "__main__":
-    write_random_data()
-    read_results("results.csv")
-
-    
+        write_result(p,i,t,r)
+    read_results()
